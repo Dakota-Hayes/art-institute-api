67cb3e7e67f27566475582f77e4e5abb8344e1f3
Dakota Hayes <dakotahayes@users.noreply.github.com>
2024-01-28 13:05:05 -0800

    push

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
 @site.route('/')
 def home():
     return render_template('index.html')
+
+@site.route('/static/main.js')
+def serve_main_js():
+    return send_from_directory('static','main.js')
